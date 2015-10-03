536564e2e285e073f1a3c2748f19cd333f135e67
Zephyre <haizi.zh@gmail.com>
2015-10-03 12:59:36 +0800

    MOD: 调整目录结构

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Zephyre'
 
 
-from .etcd import get_service
-from .etcd import get_conf
-from .etcd import build
+from .impl import get_service
+from .impl import get_conf
+from .impl import build
